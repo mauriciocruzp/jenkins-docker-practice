8641f2055a055af0d0663e82049bff1b95850913
Mauricio Cruz <92227312+mauriciocruzp@users.noreply.github.com>
2024-06-05 12:19:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hola mundo!');
+    res.send('Hola mundoooooo');
     });
 
 app.listen(port, () => {
